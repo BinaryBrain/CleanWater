ed4d93b50a2f93876b12509cd7f11edf0919acc6
Sacha Bron <sacha.bron@copperflake.com>
2015-04-11 13:57:57 +0200

    Data collection

---
+++ collections/data.js
@@ -0,0 +1,26 @@
+Data = new Mongo.Collection("data");
+Data.attachSchema(new SimpleSchema({
+	note: {
+		type: Number,
+		label: "Lab Name",
+		max: 10,
+		max: 0
+	},
+	labName: {
+		type: String,
+		label: "Lab Name",
+		max: 200
+	},
+	"measure.$.name" : {
+		type: String,
+		label: "Name"
+	},
+	"measure.$.value" : {
+		type: String,
+		label: "Value"
+	},
+	"measure.$.unit" : {
+		type: Boolean,
+		label: "Unit"
+	}
+}));
