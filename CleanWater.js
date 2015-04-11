0a37612899b22c80ecb406a2ac69d101a58cebae
Flagoul <basile.vu@gmail.com>
2015-04-11 16:00:33 +0200

    Adding pseudo-map and pseudo-button to open form

--- CleanWater.js
+++ CleanWater.js
@@ -1,9 +1,13 @@
 if (Meteor.isClient) {
-  
+	Template.body.events({
+		'click #openform': function () {
+			console.log("Must open form.");
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
 }
